befe2721f49c40055a0a855738e918e48a1aa8cc
Maksym Kupchenko <maxkup19@gmail.com>
2022-07-10 21:13:47 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Max Kup on 10.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
