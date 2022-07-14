836d763bcb27a545bf69814555cf8e384e3e6121
Maksym Kupchenko <maxkup19@gmail.com>
2022-07-14 23:40:29 +0200

    Implement Mode and ViewModel

--- Memorize/Models/MemoryGame.swift
+++ Memorize/Models/MemoryGame.swift
@@ -8,15 +8,26 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: [Card]
+    private(set) var cards: [Card]
     
     func choose(card: Card) {
         
     }
     
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        //add numberOfPaitsOfCards * 2 cards to array
+        for pairIndex in 0...numberOfPairsOfCards {
+            let content = createCardContent(pairIndex )
+            
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
         
         
--- Memorize/ViewModels/EmoijMemoryGame.swift
+++ Memorize/ViewModels/EmoijMemoryGame.swift
@@ -9,4 +9,15 @@ import SwiftUI
 
 class EmojiMemoryGame {
     
+    static let emojis = ["🚲", "🚂", "🚁", "🚜", "🚕" ,"🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚄"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { index in emojis[index]}
+    }
+    
+    private var model: MemoryGame<String> = createMemoryGame()
+    
+    var cards: Array<MemoryGame<String>.Card> { return model.cards }
+    
+    
 }
